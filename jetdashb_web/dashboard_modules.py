657050ebf5afc750e52edd94d8da0549f1dab375
Mirek Zvolský <zvolsky@seznam.cz>
2018-01-31 15:17:59 +0100

    zaloha

---
+++ jetdashb_web/dashboard_modules.py
@@ -0,0 +1,13 @@
+from jet.dashboard.modules import DashboardModule
+
+
+class RecentTickets(DashboardModule):
+    title = 'Recent tickets'
+    title_url = 'https://www.seznam.cz'
+    template = 'jetdashb_web/moje.html'
+    blabla = 'bleble'
+
+    """
+    def init_with_context(self, context):
+        self.children = []
+    """
